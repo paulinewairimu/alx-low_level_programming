7fcef3bfb3f747366c5fa8dd86d5ad4bc79b2fe6
paulinewairimu <wairimupauline759@gmail.com>
2023-12-20 18:45:09 -0800

    Fibonacci 4

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,19 +10,20 @@
 
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
-
-
-	while (k + j < 4000000)
-	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
-	}
-	printf("%ld", sum);
-	return (0);
+int limit = 4000000;
+int term1 = 1;
+int term2 = 2;
+int next;
+int sum = 2;
+while (next <= limit)
+{
+next = term1 + term2;
+if (next % 2 == 0)
+sum = sum + next;
+term1 = term2;
+term2 = next;
+}
+printf("%d\n", sum);
+return (0);
 }
 
