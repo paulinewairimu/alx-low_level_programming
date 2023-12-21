1db0252dbf1bb2da1a9bdd75bea593a0b791ebeb
paulinewairimu <wairimupauline759@gmail.com>
2023-12-20 18:00:22 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,15 @@
 #include "main.h"
 
 /**
- * main - function
- * Return: 0
+ * print_alphabet - make the alphabet
+ * Return: void
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
