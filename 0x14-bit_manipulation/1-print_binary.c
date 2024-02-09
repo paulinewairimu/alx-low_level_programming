fc583bf5ca038fbaae6a5aad7532cd1a33b9005b
paulinewairimu <wairimupauline759@gmail.com>
2024-02-08 18:35:55 -0800

    Write a function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,32 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-/*
- *
- * Prints the binary representation of a number
- * 
- * Return: nothing
- *
+/**
+ * print_binary -The binary prints  equivalent of  decimal number
+ * @n: number to print in binary
  */
-
 void print_binary(unsigned long int n)
-
 {
-	char i;
+	int a, count = 0;
+	unsigned long int current;
 
-	if (n == 0)
+	for (a = 63; a >= 0; a--)
 	{
+		current = n >> a;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
 		_putchar('0');
-		return;
-	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	print_binary(n >> 1);
-	i = (n & 1) ? '1' : '0';
-	_putchar(i);
 }
